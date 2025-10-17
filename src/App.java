6444adbb83047074b8944a1b9a48fccae85a2fe8
Rémi Bastide <Remi.Bastide@gmail.com>
2025-10-17 14:37:54 +0200

    3° commit

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         // je rajoute un commentaire
+        // pour tester
         System.out.println("Hello, World!");
     }
 }
