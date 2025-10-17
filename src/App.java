c4ceca870e88f3ca1d2d54dddc96050a63dc959d
Rémi Bastide <Remi.Bastide@gmail.com>
2025-10-17 14:59:25 +0200

    modif  gauche

--- src/App.java
+++ src/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         // je rajoute un commentaire
         // pour tester
+        // modiff à gauche
         System.out.println("Hello, World!");
     }
 }
